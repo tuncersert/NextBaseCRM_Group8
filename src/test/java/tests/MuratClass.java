b3b377ced34755ce191797c7b630d5a7ca9165a3
MrtYvz192 <wingsoverthesky@gmail.com>
2020-05-29 13:42:11 +0300

    update MuratClass.java

--- src/test/java/tests/MuratClass.java
+++ src/test/java/tests/MuratClass.java
@@ -4,5 +4,6 @@ public class MuratClass {
     public static void main(String[] args) {
         System.out.println("Murat's class");
         //just to make a difference
+        //second difference
     }
 }
