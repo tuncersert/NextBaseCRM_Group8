8bb4315212e6a6d866b10bbe8e738b8c58ca19ff
MrtYvz192 <wingsoverthesky@gmail.com>
2020-05-29 13:40:48 +0300

    add MuratClass.java

---
+++ src/test/java/tests/MuratClass.java
@@ -0,0 +1,8 @@
+package tests;
+
+public class MuratClass {
+    public static void main(String[] args) {
+        System.out.println("Murat's class");
+        //just to make a difference
+    }
+}
