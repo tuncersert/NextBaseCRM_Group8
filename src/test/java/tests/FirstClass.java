ed1afabd5af09d49238367c1a709384c0696cf67
Fatihkesik <fatihkesikfk@gmail.com>
2020-05-29 12:35:37 +0200

    add Fatihsclass.java

--- src/test/java/tests/FirstClass.java
+++ src/test/java/tests/FirstClass.java
@@ -1,7 +1,6 @@
 package tests;
 
 public class FirstClass {
-    public static void main(String[] args) {
-        System.out.println("Hello Group 8");
+    public static void main(String[] args) { System.out.println("Hello Group 8");
     }
 }
