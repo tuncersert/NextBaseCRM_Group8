983acf060ce79c494d2880c1a4ee7c36f82f05c7
meyildiz <mehmeteminyildiz83@gmail.com>
2020-05-30 14:56:52 +0200

    Mehmet class merge

--- src/test/java/tests/MehmetMergeTest.java
+++ src/test/java/tests/MehmetMergeTest.java
@@ -3,5 +3,6 @@ package tests;
 public class MehmetMergeTest {
     public static void main(String[] args) {
         System.out.println("Merge test from local working directory");
+        System.out.println("This is for producing a conflict");
     }
 }
